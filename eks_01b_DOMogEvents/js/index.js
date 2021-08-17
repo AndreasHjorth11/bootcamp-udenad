d81083f671dffe8542d033f7693f0558eca3ab58
AndreasHjorth11 <77662628+AndreasHjorth11@users.noreply.github.com>
2021-08-17 14:33:05 +0200

    Add assignment 01b

--- eks_01b_DOMogEvents/js/index.js
+++ eks_01b_DOMogEvents/js/index.js
@@ -1,6 +1,39 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+//Opretter en variabel med queryselector til at tage fat og gemme p taggets id//
+let countElement = document.querySelector('#text-count');
+//Tager variablen og ændre text contentet
+let number = 5;
+let min = 0;
+let max	= 15;
+let plusButton = document.querySelector('#btn-count-increase');
+let minusButton = document.querySelector('#btn-count-decrease');
+
+
+function updateNumber(){
+	countElement.textContent = number; 
+}
+
+updateNumber();
+
+
+//() => betyder function
+plusButton.addEventListener("click", () => {
+	if(number < max){
+		number++;
+		updateNumber(); 
+	}
+});
+
+
+//() => betyder function
+minusButton.addEventListener("click", () => {
+	if(number > min){
+		number--;
+		updateNumber();
+	}
+
+});
 
 }); // Afslutter: DOMContentLoaded
