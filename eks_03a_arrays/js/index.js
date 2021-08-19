4de0110396f4fdfad9c7c9fb97952f9a223763a4
AndreasHjorth11 <77662628+AndreasHjorth11@users.noreply.github.com>
2021-08-19 15:04:10 +0200

    Add Assignment 03a

--- eks_03a_arrays/js/index.js
+++ eks_03a_arrays/js/index.js
@@ -1,6 +1,17 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	let navn = ["Anders ", "Fedtmule ", "Mickey "];
+
+	navn.forEach(navn => {
+		let ListItem = document.createElement('li');
+		document.querySelector('#liste').appendChild(ListItem);
+		ListItem.innerHTML = navn;
+		ListItem.style.listStyle = 'none';
+		console.log(navn)
+	});
+
+
+
 
 }); // Afslutter: DOMContentLoaded
