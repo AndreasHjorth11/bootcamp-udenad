d5a15be52821e41a46640e650a05785274f6af9e
AndreasHjorth11 <77662628+AndreasHjorth11@users.noreply.github.com>
2021-08-19 11:25:38 +0200

    Add Assignment 02b

--- eks_02b_funktioner/js/index.js
+++ eks_02b_funktioner/js/index.js
@@ -1,6 +1,12 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	// Parametre er det man skriver i parantesen ved function navnet //
+	function hils(navn)
+	{
+		console.log(`Hej med dig ${navn}`);
+	}
+	// Argumenter er det man skriver parantesen under her //
+	hils("Andreas");
 
 }); // Afslutter: DOMContentLoaded
